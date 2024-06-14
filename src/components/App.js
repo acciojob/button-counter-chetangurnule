2134d018afd4f29f566b2b63219ced6911d580e9
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2024-06-14 11:10:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  function onClickHandler() {
+    setCount((prev) => prev + 1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <button className="btn" onClick={onClickHandler}>
+        click me
+      </button>
+      <p className="displayText">Button clicked {count} times</p>
     </div>
   )
 }
